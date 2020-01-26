2162a6efcba764ebb8c90bca21601ad80a3b7ec1
Olga Lenko <lenkoooo@mail.ru>
2020-01-26 21:51:58 +0300

    Lesson 1

---
+++ lesson_1.py
@@ -0,0 +1,116 @@
+# ex. 1
+
+s = input()
+if 0 < len(s) < 1000:
+    full_name = s.split()
+    name = full_name[0]
+    surname = full_name[1]
+    print(name.capitalize(), surname.capitalize())
+    
+# OR, for full name including more than two words:
+
+s = input()
+if 0 < len(s) < 1000:
+    full_name = s.split()
+    for i in full_name:
+        print(i.capitalize(), end = ' ')
+
+# ex. 2
+
+string = input()
+result = {}
+for i in string:
+    result[i] = string.count(i)
+print(result)
+
+# ex. 3
+
+string = input()
+if len(string) < 2:
+    print('Empty String')
+else:
+    new_string = string[0:2] + string[-2:]
+    print(new_string)
+
+
+# ex. 4
+
+list = input().split()
+count = 0
+for string in list:
+    if len(string) >= 2 and string[0] == string[-1]:
+        count += 1
+    else:
+        continue
+print(count)
+
+# ex. 5
+
+set1 = set(input().split())
+set2 = set(input().split())
+set3 = set(input().split())
+if set3.issubset(set1) and set3.issubset(set2):
+    print("True")
+else:
+    print("False")
+
+# ex. 6
+
+n = int(input())
+new_dict = {}
+i = 1
+while i <= n:
+    new_dict[i] = i*i
+    i += 1
+print(new_dict)
+
+# ex. 7
+
+dict1 = {'cat' : 'Vaska', 'second_cat' : 'Tishka'}
+dict2 = {'dog' : 'Robert', 'hamster' : 'Marfusha', 'cat': 'Murzik'}
+
+for key1, value1 in dict1.items():
+    for key2, value2 in dict2.items():
+        if key1 == key2:
+            dict2[key2] = value1, value2
+dict1.update(dict2)
+print(dict1)
+
+# ex. 8
+
+salary = {
+    'salary_jan' : 12020, 
+    'salary_feb' : 45000, 
+    'salary_mar' : 68000,
+    'salary_apr' : 15900,
+    'salary_may' : 28200,
+    'salary_jun' : 41000
+    }
+salary = sorted(salary.values())
+salary_i_like = salary[-1:-4:-1]
+print(*salary_i_like)
+
+# ex. 9
+
+input_list = input().split()
+new = set(input_list)
+
+print(*new)
+
+# OR
+list = input().split()
+list.sort()
+list.append(0)
+for i in range(len(list)-1):
+    if list[i] == list[i+1]:
+        i += 1
+    else:
+        print(list[i], end = ' ')
+        i += 1
+        
+# ex. 10
+
+list1 = input().split()
+list2 = input().split()
+difference = list(set(list1)^set(list2))
+print(*difference)
---
+++ lesson_2.py
@@ -0,0 +1,61 @@
+# ex. 1
+
+def sq(a, x):
+    res = []
+    count = 0
+    for i in range(0, x+1):
+        if i % 2 != 0:
+            res += [i**2]
+            count += 1
+            i += 1
+    print(*res, 'всего чисел -', count)
+sq(0, 9)
+    
+# ex. 2
+
+def div_by_c(a, b, c):
+    count = 0
+    for i in range(a+1, b):
+        if i % c == 0:
+            count += 1
+            i += 1
+    print(count)
+div_by_c(-11, 12, 4)
+
+# ex. 3
+
+def fact(a):
+    result = 1
+    for i in range(2, a+1):
+        result *= i
+        i += 1
+    print(result)
+
+# ex. 4
+
+def old_range(a, b):
+    range_list = []
+    for i in range(a, b):
+        range_list += [i]
+        i += 1
+    print(range_list)
+
+# ex. 5
+
+name_password = {
+    'Vasyliy' : 123456,
+    '123ya' : '123ya',
+    'Angelina' : 'porosenochek'
+    }
+    
+name = input("Введите имя пользователя ")
+password = input("Введите пароль ")
+
+for key, value in name_password.items():
+    if name_password[name] == password:
+        print("Password for user:", name, "is correct")
+        break
+    else:
+        print("Password for user:", name, "is incorrect")
+        print("Please try again...")
+        password = input("Введите пароль ")
